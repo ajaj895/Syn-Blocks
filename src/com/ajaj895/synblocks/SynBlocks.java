830815738b78d603bf6db6b24313ca55717ebf81
Evan <41348743+ajaj895@users.noreply.github.com>
2020-05-23 18:14:38 -0500

    Initial commit and prototype GUI
    Here is the initial commit for the Syn-Blocks and a prototype GUI for the code.org/scratch style of block coding. Also included is the first jar build in the dist directory.

---
+++ src/com/ajaj895/synblocks/SynBlocks.java
@@ -0,0 +1,23 @@
+// This is the main class for the Syn-Blocks project for the OpenHacks hackathon.
+// Written by Evan Colwell, May 23, 2020.
+// This program should only just call the GUI, the GUI will handle the interactions with a backend style system handling the blocks and the actual code.
+package com.ajaj895.synblocks;
+
+import com.ajaj895.synblocks.gui.Gui;
+
+/**
+ *
+ * @author Evan Colwell
+ */
+public class SynBlocks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a test");//for testing purposes
+        Gui gui = new Gui();//creates the GUI object.
+        gui.setVisible(true);//makes the GUI visable.
+    }
+    
+}
